9f25ccdeb80477dfc894cfddf8c5d6d8efc4a4e2
navah <navah@uchicago.edu>
2013-09-04 14:40:21 -0500

    cleaned up phreeqout input and output

--- phreeqout/phreeqout.f90
+++ phreeqout/phreeqout.f90
@@ -15,82 +15,76 @@ program phreeqout
     INCLUDE "IPhreeqc.f90.inc"
   INTEGER(KIND=4) :: id
   INTEGER(KIND=4) :: i
-  CHARACTER(LEN=80) :: line
+  CHARACTER(LEN=400) :: line
+  character(len=800) :: inputz
+  real(8), allocatable :: outmat(:,:)
+
+  ! PHREEQC INPUT SCRIPT 
+  inputz = "SOLUTION 1 Pure water" //NEW_LINE('')// &
+  &"    pH 7.0" //NEW_LINE('')// &
+  &"    temp 25.0" //NEW_LINE('')// &
+  &"EQUILIBRIUM_PHASES 1" //NEW_LINE('')// &
+  &"    Gypsum 0.0 1.0" //NEW_LINE('')// &
+  &"    Anhydrite 0.0 1.0" //NEW_LINE('')// &
+  &"REACTION_TEMPERATURE 1" //NEW_LINE('')// &
+  &"    25.0 75.0 in 51 steps" //NEW_LINE('')// &
+!  &"SAVE solution 1" //NEW_LINE('')// &
+!  &"SAVE equilibrium_phases 1" //NEW_LINE('')// &
+  &"SELECTED_OUTPUT" //NEW_LINE('')// &
+  &"    -reset false" //NEW_LINE('')// &
+  &"    -si anhydrite gypsum" //NEW_LINE('')// &
+  &"END"
+  write(*,*) inputz
 
   id = CreateIPhreeqc()
   IF (id.LT.0) THEN
      STOP
   END IF
-
-  IF (SetDumpStringOn(id, .TRUE.).NE.IPQ_OK) THEN
-     CALL OutputErrorString(id)
-     STOP
-  END IF
-
-  IF (LoadDatabase(id, "phreeqc.dat").NE.0) THEN
-     CALL OutputErrorString(id)
-     STOP
-  END IF
-
-  IF (AccumulateLine(id, "SOLUTION 1 Pure water").NE.IPQ_OK) THEN
-     CALL OutputErrorString(id)
-     STOP
-  END IF
-
-  IF (AccumulateLine(id, "EQUILIBRIUM_PHASES 1").NE.IPQ_OK) THEN
-     CALL OutputErrorString(id)
-     STOP
-  END IF
-
-  IF (AccumulateLine(id, "    Calcite 0 10").NE.IPQ_OK) THEN
-     CALL OutputErrorString(id)
-     STOP
-  END IF
-
-  IF (AccumulateLine(id, "SAVE solution 1").NE.IPQ_OK) THEN
-     CALL OutputErrorString(id)
-     STOP
-  END IF
-
-  IF (AccumulateLine(id, "SAVE equilibrium_phases 1").NE.IPQ_OK) THEN
-     CALL OutputErrorString(id)
-     STOP
-  END IF
-
-  IF (AccumulateLine(id, "DUMP").NE.IPQ_OK) THEN
-     CALL OutputErrorString(id)
-     STOP
-  END IF
-
-  IF (AccumulateLine(id, "    -solution 1").NE.IPQ_OK) THEN
-     CALL OutputErrorString(id)
-     STOP
-  END IF
-
-  IF (AccumulateLine(id, "    -equilibrium_phases 1").NE.IPQ_OK) THEN
-     CALL OutputErrorString(id)
-     STOP
-  END IF
-
-  WRITE(*,*) "Input:"
-  CALL OutputAccumulatedLines(id)
-
-  IF (RunAccumulated(id).NE.0) THEN
-     CALL OutputErrorString(id)
-     STOP
-  END IF
-
-  WRITE(*,*) "Dump:"
-  DO i=1,GetDumpStringLineCount(id)
-     CALL GetDumpStringLine(id, i, line)
-     WRITE(*,*) TRIM(line)
-  END DO
-
-  IF (DestroyIPhreeqc(id).NE.IPQ_OK) THEN
+  
+  IF (SetSelectedOutputStringOn(id, .TRUE.).NE.IPQ_OK) THEN
      CALL OutputErrorString(id)
      STOP
   END IF
   
+  IF (LoadDatabase(id, "phreeqc.dat").NE.0) THEN
+     CALL OutputErrorString(id)
+     STOP
+  END IF
+  
+  IF (RunString(id, inputz).NE.0) THEN
+     CALL OutputErrorString(id)
+     STOP
+  END IF
+  
+  WRITE(*,*) "WRITING TO 2D ARRAY AND OUTPUT FILES"
+  
+  ! OPEN FILE
+  OPEN(UNIT=12, FILE="outmat.txt", ACTION="write", STATUS="replace") 
+  
+  ! WRITE AWAY
+  allocate(outmat(GetSelectedOutputStringLineCount(id),2))
+  DO i=1,GetSelectedOutputStringLineCount(id)
+     call GetSelectedOutputStringLine(id, i, line)
+     ! HEADER BITS YOU MAY WANT
+     if (i .eq. 1) then
+     	write(12,*) trim(line)
+     end if
+     ! MEAT
+     if (i .gt. 1) then
+     	read(line,*) outmat(i,:)
+     	write(12,"(2F10.5)") outmat(i,1), outmat(i,2)
+     end if
+  END DO
+  
+  IF (DestroyIPhreeqc(id).NE.IPQ_OK) THEN
+     STOP
+  END IF
+
+  
   write(*,*) "phreequm dress"
+  
+  
     
 end program phreeqout
+
+
