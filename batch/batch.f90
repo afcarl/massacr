0d21ec959c27a5f9be1c333ca86721834471f11f
navah <navah@uchicago.edu>
2014-02-09 13:53:42 -0600

    CO2 diffusion workaround brought to you by trina

--- batch/batch.f90
+++ batch/batch.f90
@@ -50,7 +50,7 @@ program batch
   inputz0 = "SOLUTION 1 " //NEW_LINE('')// &
   &"    pH 8.22" //NEW_LINE('')// &
   &"    units   ppm" //NEW_LINE('')// &
-  &"    temp 15.0" //NEW_LINE('')// &
+  &"    temp 10.0" //NEW_LINE('')// &
   &"    Ca 412.3" //NEW_LINE('')// &
   &"    Mg 1291.8" //NEW_LINE('')// &
   &"    Na 10768.0" //NEW_LINE('')// &
@@ -65,9 +65,30 @@ program batch
   &"    Alkalinity 141.682 as HCO3" //NEW_LINE('')// &
   &"    -water		.5	# kg" //NEW_LINE('')// &
   &"END" //NEW_LINE('')// &
+ 
+!     &"SOLUTION 2 " //NEW_LINE('')// &
+!     &"    pH 8.22" //NEW_LINE('')// &
+!     &"    units   ppm" //NEW_LINE('')// &
+!     &"    temp 10.0" //NEW_LINE('')// &
+!     &"    Ca 412.3" //NEW_LINE('')// &
+!     &"    Mg 1291.8" //NEW_LINE('')// &
+!     &"    Na 10768.0" //NEW_LINE('')// &
+!     &"    K 399.1" //NEW_LINE('')// &
+!     &"    Fe .002" //NEW_LINE('')// &
+!     &"    Mn .0002" //NEW_LINE('')// &
+!     &"    S(6) 2712.0 as SO4" //NEW_LINE('')// &
+!     &"    Si 4.28" //NEW_LINE('')// &
+!     &"    Cl 19353.0" //NEW_LINE('')// &
+!     &"    NO3 0.29" //NEW_LINE('')// &
+!     &"    NH4 0.03" //NEW_LINE('')// &
+!     &"    Alkalinity 141.682 as HCO3" //NEW_LINE('')// &
+!     &"    -water		.5	# kg" //NEW_LINE('')// &
+!     &"END" //NEW_LINE('')// &
+
   
   &"EQUILIBRIUM_PHASES 1" //NEW_LINE('')// &
-  &"    CO2(g) -2.0 100" //NEW_LINE('')// &
+!  &"    CO2(g) -3.45 10000" //NEW_LINE('')// &
+!  &"    O2(g) -.69897 10000" //NEW_LINE('')// &
 !  &"    Siderite 0.0 0.0" //NEW_LINE('')// &
   &"    Kaolinite 0.0 0.0" //NEW_LINE('')// &
   &"    Goethite 0.0 0.0" //NEW_LINE('')// &
@@ -85,6 +106,11 @@ program batch
   &"    Clinoptilolite-Ca 0.0 0.0" //NEW_LINE('')// &
   &"    Pyrite 0.0 0.0" //NEW_LINE('')// &
   &"    Quartz 0.0 0.0" //NEW_LINE('')// &
+  &"    K-Feldspar 0.0 0.0" //NEW_LINE('')// &
+  
+!   &"EQUILIBRIUM_PHASES 2" //NEW_LINE('')// &
+!   &"    CO2(g) -3.45 10000" //NEW_LINE('')// &
+!   &"    O2(g) -.69897 10000" //NEW_LINE('')// &
 
   &"CALCULATE_VALUES" //NEW_LINE('')// &
   
@@ -98,7 +124,7 @@ program batch
   &"+ EQUI('Dolomite')*184.3/2.84 + EQUI('Smectite-high-Fe-Mg')*425.7/2.7" // &
   &"+ EQUI('Dawsonite')*144.0/2.42 + EQUI('Magnesite')*84.3/3.0" // &
   &"+ EQUI('Siderite')*115.8/3.96 + EQUI('Calcite')*100.0/2.71" // &
-  &"+ EQUI('Quartz')*60.0/2.62" // &
+  &"+ EQUI('Quartz')*60.0/2.62 + EQUI('K-Feldspar')*193.0/2.56" // &
   &"+ KIN('Plagioclase')*270.0/2.68 + KIN('Augite')*230.0/3.4" // &
   &"+ KIN('Pigeonite')*239.6/3.38 + KIN('Magnetite')*231.0/5.15" // &
   &"+ KIN('BGlass')*46.5/2.92" // &
@@ -110,8 +136,6 @@ program batch
   &"R(phi)" //NEW_LINE('')// &
   &"-start" //NEW_LINE('')// &
   &"10 phi = 1.0-(CALC_VALUE('R(sum)')/(CALC_VALUE('R(sum)')+(TOT('water')*1000.0)))" //&
-  !&"10 phi = GET_POR(1)" //&
-  !&"" //NEW_LINE('')// &
   !&"10 phi = 0.1" //&
   &"" //NEW_LINE('')// &
   &"100 SAVE phi" //NEW_LINE('')// &
@@ -136,7 +160,7 @@ program batch
   &"+ EQUI('Dolomite')*2.84 + EQUI('Smectite-high-Fe-Mg')*2.7" // &
   &"+ EQUI('Dawsonite')*2.42 + EQUI('Magnesite')*3.0" // &
   &"+ EQUI('Siderite')*3.96 + EQUI('Calcite')*2.71" // &
-  &"+ EQUI('Quartz')*2.62" // &
+  &"+ EQUI('Quartz')*2.62 + EQUI('k-Feldspar')*2.56" // &
   &"+ KIN('Plagioclase')*2.68 + KIN('Augite')*3.4" // &
   &"+ KIN('Pigeonite')*3.38 + KIN('Magnetite')*5.15" // &
   &"+ KIN('BGlass')*2.92" //NEW_LINE('')// &
@@ -148,7 +172,7 @@ program batch
   &"+ EQUI('Dolomite') + EQUI('Smectite-high-Fe-Mg')" // &
   &"+ EQUI('Dawsonite') + EQUI('Magnesite')" // &
   &"+ EQUI('Siderite') + EQUI('Calcite')" // &
-  &"+ EQUI('Quartz')" // &
+  &"+ EQUI('Quartz') + EQUI('K-Feldspar')" // &
   &"+ KIN('Plagioclase') + KIN('Augite')" // &
   &"+ KIN('Pigeonite') + KIN('Magnetite')" // &
   &"+ KIN('BGlass'))" //NEW_LINE('')// &
@@ -175,8 +199,17 @@ program batch
   &"100 SAVE s_sp" //NEW_LINE('')// &
   &"-end" //NEW_LINE('')// &
   
-  
-
+!   &"TRANSPORT 3" //NEW_LINE('')// &
+!   &"-cells 2" //NEW_LINE('')// &
+!   &"-shifts 1" //NEW_LINE('')// &
+!   &"-time_step 1.0e14 second" //NEW_LINE('')// &
+!   &"-flow_direction diffusion_only" //NEW_LINE('')// &
+!   &"-diffusion_coefficient 7.0e-9" //NEW_LINE('')// &
+!   &"-stagnant 1" //NEW_LINE('')// &
+!   &"" //NEW_LINE('')// &
+!   &"" //NEW_LINE('')// &
+!   &"" //NEW_LINE('')// &
+!   &"" //NEW_LINE('')// &
   
   &"KINETICS" //NEW_LINE('')// &
   &"Plagioclase" //NEW_LINE('')// &
@@ -191,8 +224,11 @@ program batch
   &"-f Ca 0.015 Fe 0.095 Mg 0.065 " //&
   & "Na 0.025 K 0.01 Al 0.105 Si 0.5 S 0.003 O 1.35" //NEW_LINE('')// &
   &"-m0 96.77" //NEW_LINE('')// &
+  &"pCO2" //NEW_LINE('')// &
+  &"-f CO2" //&
+  &"-m0 10.0" //NEW_LINE('')// &
 
-  &"    -step 3.14e14 in 100" //NEW_LINE('')// &
+  &"    -step 3.14e13 in 100" //NEW_LINE('')// &
 
   &"INCREMENTAL_REACTIONS true" //NEW_LINE('')// &
   
@@ -209,9 +245,9 @@ program batch
   
   &"Plagioclase" //NEW_LINE('')// &
   &"-start" //NEW_LINE('')// &
-  &"    10 rate = (1-SR('Plagioclase'))*M*270.0*CALC_VALUE('R(s_sp)')*0.1*(((1.58e-9)"//&
+  &"    10 rate = (1-SR('Plagioclase'))*M*270.0*CALC_VALUE('R(s_sp)')*0.1*((1.58e-9)"//&
   &"*exp(-53.5/(.008314*TK))*(ACT('H+')^0.541) +(3.39e-12)*exp(-57.4/(.008314*TK)) +"//&
-  &"(4.78e-15)*exp(-59.0/(.008314*TK))*(ACT('H+'))^-0.57))"//NEW_LINE('')//&
+  &"(4.78e-15)*exp(-59.0/(.008314*TK))*(ACT('H+')^-0.57))"//NEW_LINE('')//&
   &"    20 save rate * time"//NEW_LINE('')//&
   &"-end" //NEW_LINE('')// &
   
@@ -236,7 +272,11 @@ program batch
   &"    20 save rate0 * time" //NEW_LINE('')// &
   &"-end" //NEW_LINE('')// &
 
-
+  &"pCO2" //NEW_LINE('')// &
+  &"-start" //NEW_LINE('')// &
+  &"    10 rate0 = 1.0e-12" //NEW_LINE('')// &
+  &"    20 save rate0 * time" //NEW_LINE('')// &
+  &"-end" //NEW_LINE('')// &
    
   &"DUMP" //NEW_LINE('')// &
   &"    -solution 1" //NEW_LINE('')// &
@@ -246,10 +286,11 @@ program batch
   &"    -reset false" //NEW_LINE('')// &
   &"    -k plagioclase augite pigeonite magnetite bglass" //NEW_LINE('')// &
   &"    -ph" //NEW_LINE('')// &
-  &"    -molalities HCO3-" //NEW_LINE('')// &
+  &"    -alkalinity" //NEW_LINE('')// &
+!  &"    -molalities HCO3-" //NEW_LINE('')// &
   &"    -p stilbite sio2(am) kaolinite albite saponite-mg celadonite Clinoptilolite-Ca" //NEW_LINE('')// &
   &"    -p pyrite hematite goethite dolomite Smectite-high-Fe-Mg Dawsonite" //NEW_LINE('')// &
-  &"    -p magnesite siderite calcite quartz" //NEW_LINE('')// &
+  &"    -p magnesite siderite calcite quartz k-feldspar" //NEW_LINE('')// &
   &"    -calculate_values R(phi) R(s_sp) R(water_volume) R(rho_s)" //NEW_LINE('')// &
   &"    -time" //NEW_LINE('')// &
   &"END"
@@ -300,7 +341,7 @@ program batch
   OPEN(UNIT=12, FILE="testMat.txt", ACTION="write", STATUS="replace") 
   
   ! WRITE AWAY
-  allocate(outmat(GetSelectedOutputStringLineCount(id)+1,51))
+  allocate(outmat(GetSelectedOutputStringLineCount(id)+1,53))
   DO i=1,GetSelectedOutputStringLineCount(id)
      call GetSelectedOutputStringLine(id, i, line)
      ! HEADER BITS YOU MAY WANT
--- batch/plotBatch.py
+++ batch/plotBatch.py
@@ -12,8 +12,8 @@ out = np.loadtxt('testMat.txt')
 
 fig=plt.figure()
 
-plt.rc('xtick', labelsize=8) 
-plt.rc('ytick', labelsize=8)
+plt.rc('xtick', labelsize=6) 
+plt.rc('ytick', labelsize=6)
 
 # 3 stillbite 832.2
 mStil = 832.2
@@ -49,14 +49,17 @@ mSid = 115.8
 mCal = 100.0
 # 35 quartz
 mQua = 60.0
+# 37 k-feldspar
+mKspar = 193.0
 
-mass = out[:,37]*270.0 + out[:,39]*230.0 + out[:,41]*238.6 +  \
-       out[:,43]*231.0 + out[:,45]*46.5 + \
+mass = out[:,39]*270.0 + out[:,41]*230.0 + out[:,43]*238.6 +  \
+       out[:,45]*231.0 + out[:,47]*46.5 + \
        out[:,3]*mStil + out[:,5]*mSi + out[:,7]*mKao + out[:,9]*mAlb +\
        out[:,11]*mSap + out[:,13]*mCel +\
        out[:,15]*mClin + out[:,17]*mPyr + out[:,19]*mHem + out[:,21]*mGoe +\
        out[:,23]*mDol + out[:,25]*mSmec + out[:,27]*mDaws + \
-       out[:,29]*mMagn + out[:,31]*mSid + out[:,33]*mCal + out[:,35]*mQua
+       out[:,29]*mMagn + out[:,31]*mSid + out[:,33]*mCal + out[:,35]*mQua +\
+       out[:,37]*mKspar
 
 #mass = 1.0
 out[:,0] = out[:,0]/(3.14e7)
@@ -64,16 +67,16 @@ out[:,0] = out[:,0]/(3.14e7)
 print out.shape
 
 print "phi"
-print out[:,47]
-
-print "s_sp"
-print out[:,48]
-
-print "water_volume"
 print out[:,49]
 
-print "rho_s"
+print "s_sp"
 print out[:,50]
+
+print "water_volume"
+print out[:,51]
+
+print "rho_s"
+print out[:,52]
 
 
 
@@ -82,7 +85,7 @@ print out[:,50]
 # precipitates #
 ################
 
-ax = plt.subplot(2,2,1)
+ax = plt.subplot(2,3,1)
 this = out[:,1]
 p3, = ax.plot(out[:,0],out[:,3]*mStil/mass, 'b:', label='Stilbite',linewidth=2)
 p3, = ax.plot(out[:,0],out[:,5]*mSi/mass, 'b', label='SiO2',linewidth=2)
@@ -99,44 +102,53 @@ p3, = ax.plot(out[:,0],out[:,17]*mPyr/mass, 'r--', label='Pyrite',linewidth=2)
 ##p3, = ax.plot(out[:,0],out[:,25]*230.0/mass, 'k:', label='',linewidth=1)
 ##p3, = ax.plot(out[:,0],out[:,27]*230.0/mass, 'k:', label='',linewidth=1)
 ##p3, = ax.plot(out[:,0],out[:,29]*230.0/mass, 'k:', label='',linewidth=1)
-p3, = ax.plot(out[:,0],out[:,35]*mQua/mass, 'k:', label='Quartz',linewidth=1)
+p3, = ax.plot(out[:,0],out[:,35]*mQua/mass, 'k:', label='Quartz',linewidth=2)
 #p3, = ax.plot(out[:,0],out[:,33]*230.0/mass, 'k', label='calcite',linewidth=3)
+p3, = ax.plot(out[:,0],out[:,37]*mQua/mass, 'grey', label='K-Spar',linewidth=2)
 
 
 handles, labels = ax.get_legend_handles_labels()
 plt.legend(handles[::-1], labels[::-1])
 
-plt.ylabel('MASS FRACTION',fontsize=8)
-plt.xlabel('TIME [yr]',fontsize=8)
+plt.ylabel('MASS FRACTION',fontsize=6)
+plt.xlabel('TIME [yr]',fontsize=6)
+plt.legend(handles, labels,loc=2,prop={'size':5}, ncol=2)
+plt.title('SECONDARY MINERAL PRECIPITATION',fontsize=8)
+
+
+############
+# minerals #
+############
+
+ax = plt.subplot(2,3,2)
+p3, = ax.plot(out[:,0],out[:,39]/max(out[:,39]), 'b', label='Plagioclase',linewidth=2)
+p3, = ax.plot(out[:,0],out[:,41]/max(out[:,41]), 'r', label='Augite',linewidth=2)
+p3, = ax.plot(out[:,0],out[:,43]/max(out[:,43]), 'g', label='Pigeonite',linewidth=2)
+
+plt.ylabel('NORMALIZED AMOUNT [mol]',fontsize=6)
+plt.xlabel('TIME [yr]',fontsize=6)
+plt.title('MINERALS',fontsize=8)
+
+
+handles, labels = ax.get_legend_handles_labels()
+plt.legend(handles[::-1], labels[::-1])
 plt.legend(handles, labels,loc=2,prop={'size':6}, ncol=2)
-plt.title('SECONDARY MINERAL PRECIPITATION',fontsize=10)
 
-############
-# porosity #
-############
-
-ax = plt.subplot(2,2,2)
-p3, = ax.plot(out[:,0],out[:,47], 'b:', label='Porosity',linewidth=2)
-
-plt.ylabel('VOLUME FRACTION',fontsize=8)
-plt.xlabel('TIME [yr]',fontsize=8)
-plt.title('POROSITY',fontsize=10)
 
 
 ###############
 # dissolution #
 ###############
 
-ax = plt.subplot(2,2,3)
+ax = plt.subplot(2,3,3)
 
 
 
-
-p3, = ax.plot(out[:,0],out[:,37]*270.0/mass, 'r', label='Plagioclase',linewidth=2)
-p3, = ax.plot(out[:,0],out[:,39]*230.0/mass, 'g', label='Augite',linewidth=2)
-p3, = ax.plot(out[:,0],out[:,41]*238.6/mass, 'm--', label='Pigeonite',linewidth=2)
-p3, = ax.plot(out[:,0],out[:,43]*231.0/mass, 'gold', label='Magnetite',linewidth=2)
-p3, = ax.plot(out[:,0],out[:,45]*46.5/mass, 'k', label='Basaltic Glass',linewidth=2)
+p3, = ax.plot(out[:,0],out[:,39]*270.0/mass, 'r', label='Plagioclase',linewidth=2)
+p3, = ax.plot(out[:,0],out[:,41]*230.0/mass, 'g', label='Augite',linewidth=2)
+p3, = ax.plot(out[:,0],out[:,43]*238.6/mass, 'm--', label='Pigeonite',linewidth=2)
+p3, = ax.plot(out[:,0],out[:,45]*231.0/mass, 'gold', label='Magnetite',linewidth=2)
+p3, = ax.plot(out[:,0],out[:,47]*46.5/mass, 'k', label='Basaltic Glass',linewidth=2)
 plt.grid(True)
 
 
@@ -144,26 +156,10 @@ plt.grid(True)
 handles, labels = ax.get_legend_handles_labels()
 plt.legend(handles[::-1], labels[::-1])
 plt.yticks(np.arange(0,.5,.05))
-plt.ylabel('MASS FRACTION',fontsize=8)
-plt.xlabel('TIME [yr]',fontsize=8)
-plt.legend(handles, labels,loc='best',prop={'size':6}, ncol=2)
-plt.title('PRIMARY BASALT DISSOLUTION',fontsize=10)
-
-
-###########
-# pH plot #
-###########
-
-##ax = plt.subplot(2,2,1)
-##this = out[:,1]
-##p3, = ax.plot(out[:,0],out[:,1], 'r', label='pH',linewidth=2)
-##plt.grid(True)
-##
-##plt.ylim(3,8)
-##plt.yticks(np.arange(3,8,.5))
-##plt.ylabel('pH',fontsize=8)
-##plt.xlabel('TIME [yr]',fontsize=8)
-##plt.title('HYDROTHERMAL FLUID pH',fontsize=10)
+plt.ylabel('MASS FRACTION',fontsize=6)
+plt.xlabel('TIME [yr]',fontsize=6)
+plt.legend(handles, labels,loc='best',prop={'size':5}, ncol=2)
+plt.title('PRIMARY BASALT DISSOLUTION',fontsize=8)
 
 
 
@@ -171,22 +167,57 @@ plt.title('PRIMARY BASALT DISSOLUTION',fontsize=10)
 #  carbonates  #
 ################
 
-ax = plt.subplot(2,2,4)
+ax = plt.subplot(2,3,4)
 this = out[:,1]
-p3, = ax.plot(out[:,0],out[:,31]*mSid/mass, 'm--', label='Siderite',linewidth=2)
-p3, = ax.plot(out[:,0],out[:,33]*mCal/mass, 'g--', label='Calcite',linewidth=2)
+p3, = ax.plot(out[:,0],out[:,31]*mSid/mass, 'm', label='Siderite',linewidth=2)
+p3, = ax.plot(out[:,0],out[:,33]*mCal/mass, 'g', label='Calcite',linewidth=2)
 
 handles, labels = ax.get_legend_handles_labels()
 #plt.legend(handles[::-1], labels[::-1])
 
-plt.ylabel('MASS FRACTION',fontsize=8)
-plt.xlabel('TIME [yr]',fontsize=8)
+plt.ylabel('MASS FRACTION',fontsize=6)
+plt.xlabel('TIME [yr]',fontsize=6)
 #plt.legend(handles, labels,loc='best',prop={'size':8})
-plt.title('PRECIPITATED CARBONATES',fontsize=10)
+plt.title('PRECIPITATED CARBONATES',fontsize=8)
+
+
+
+###########
+# pH plot #
+###########
+
+ax = plt.subplot(2,3,5)
+this = out[:,1]
+p3, = ax.plot(out[:,0],out[:,1], 'r', label='pH',linewidth=2)
+plt.grid(True)
+
+#plt.ylim(3,8)
+#plt.yticks(np.arange(3,8,.5))
+plt.ylabel('pH',fontsize=6)
+plt.xlabel('TIME [yr]',fontsize=6)
+plt.title('HYDROTHERMAL FLUID pH',fontsize=8)
+
+
+
+############
+# porosity #
+############
+
+ax = plt.subplot(2,3,6)
+p3, = ax.plot(out[:,0],out[:,49], 'k', label='Porosity',linewidth=2)
+
+plt.ylabel('VOLUME FRACTION',fontsize=6)
+plt.xlabel('TIME [yr]',fontsize=6)
+plt.title('POROSITY',fontsize=8)
+
+
+handles, labels = ax.get_legend_handles_labels()
+plt.legend(handles[::-1], labels[::-1])
+plt.legend(handles, labels,loc=2,prop={'size':6}, ncol=2)
 
 
 
 
-plt.subplots_adjust(hspace=.3, wspace=.3)
+plt.subplots_adjust(hspace=.3, wspace=.4)
 
-plt.savefig('plotBatch.png')
+plt.savefig('plotBatch.png', dpi = (200))
